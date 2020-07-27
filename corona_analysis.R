0a458836b8860884d3ef8036709d441a90d0bb5d
NiNumber <n.reifler@posteo.ch>
2020-07-27 12:47:36 +0200

    tidying up data entry fixing some bugs in corona app analysis data update

--- corona_analysis.R
+++ corona_analysis.R
@@ -1,25 +1,10 @@
+load("corona_all_data.RData")
+
+from <- 7 # number of days in the past
+to <- 0 # if 0: today
+
 # Mittelwert ueber die letzten Tage
-mean(corona_CH$CHtaegl[c((length(corona_CH$datum)-14):length(corona_CH$datum))])
+mean(corona_CH$CHtaegl[c((length(corona_CH$datum)-from):(length(corona_CH$datum)-to))])
 
 # Median
-median(corona_CH$CHtaegl[c((length(corona_CH$datum)-14):length(corona_CH$datum))])
-
-# Residuenanalyse
-par(mfrow = c(2,1))
-CHtaegl_rest <- CHtaegl - CHtaegl_loess
-ts.CHtaegl_rest <- ts(CHtaegl_rest, start = c(2020, 02, 26), frequency = 7)
-
-plot(ts.CHtaegl_rest)
-plot(corona_taegl$CHtaeglich)
-fit.CHtaegl <- ets(CHtaegl ~ datum + day, data = corona_taegl)
-summary(fit.CHtaegl)
-drop1(fit.CHtaegl, test = "F")
-plot(fit.CHtaegl)
-
-par(mfrow = c(1,1))
-plot(log(ts.CHtaegl_rest+1000))
-plot(stl(x = ts.CHtaegl_rest, s.window = 7))
-# Autocorrelation
-
-acf(ts.CHtaegl_rest)
-
+median(corona_CH$CHtaegl[c((length(corona_CH$datum)-from):(length(corona_CH$datum)-to))])
